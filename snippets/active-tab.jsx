4d89364d91d45684d39dee72e539cfb35e859855
Max Shepel <max@undeletable.name>
2024-09-16 23:57:08 +0300

    Add active tab snippet

---
+++ snippets/active-tab.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const TABS = [
+    {
+        id: "tab0",
+        title: "First tab",
+        body: "Contents of the first tab"
+    },
+    {
+        id: "tab1",
+        title: "Second tab",
+        body: "Contents of the second tab"
+    },
+    {
+        id: "tab2",
+        title: "Third tab",
+        body: "Contents of the third tab"
+    }
+];
+
+const TabsView = () => {
+    const [activeTab, setActiveTab] = useState();
+
+    return (
+        <>
+            <aside>
+                <ul>
+                    {TABS.map(tab => (
+                        <li>
+                            <button onClick={() => setActiveTab(tab)}>
+                                {tab.title}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </aside>
+            <main>
+                <h3>{activeTab.title}</h3>
+                <p>{activeTab.body}</p>
+            </main>
+        </>
+    );
+};
